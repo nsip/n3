0f1c0e23cc7f870b566bc53817013b83ab0498ea
Scott Penrose <scottp@dd.com.au>
2019-08-09 17:23:17 +1000

    Basic index page

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@
 #   * n3-client
 #   * n3-transport
 
+echo "N3BUILD: Start"
+
 DCUI_BRANCH="master"
 N3CLIENT_BRANCH="privacy-dev"
 N3TRANSPORT_BRANCH="qing-privacy-dev"
@@ -17,7 +19,7 @@ N3TRANSPORT_BRANCH="qing-privacy-dev"
 set -e
 ORIGINALPATH=`pwd`
 
-echo "removing existing builds"
+echo "N3BUILD NOTE: removing existing builds"
 rm -rf build
 
 mkdir -p build
@@ -25,7 +27,7 @@ mkdir -p build/www
 mkdir -p build/n3-client
 mkdir -p build/n3-transport
 
-echo "Creating DC-UI files"
+echo "N3BUILD: Creating DC-UI files @ $DCUI_BRANCH"
 cd ../DC-UI
 git checkout $DCUI_BRANCH
 git pull
@@ -34,16 +36,17 @@ npm install
 cd $ORIGINALPATH
 rsync -av ../DC-UI/dist/spa-mat/* build/www/dc/
 
-echo "Creating N3-Client"
+echo "N3BUILD: Creating N3-Client @ $N3CLIENT_BRANCH"
 cd $GOPATH/src/github.com/nsip/n3-client
 git checkout $N3CLIENT_BRANCH
 git pull
 ./build.sh
 cd $ORIGINALPATH
 rsync -av $GOPATH/src/github.com/nsip/n3-client/build/* build/n3-client/
-# XXX mv www/* to www/service?
+# XXX name for this www code?
+rsync -av $GOPATH/src/github.com/nsip/n3-client/www/* build/www/service/
 
-echo "Creating N3-Transport"
+echo "N3BUILD: Creating N3-Transport @ $N3TRANSPORT_BRANCH"
 cd $GOPATH/src/github.com/nsip/n3-transport
 git checkout $N3TRANSPORT_BRANCH
 git pull
@@ -51,3 +54,8 @@ mkdir build
 ./build.sh
 cd $ORIGINALPATH
 rsync -av $GOPATH/src/github.com/nsip/n3-transport/build/* build/n3-transport/
+
+echo "N3BUILD: Generating/Updating WWW files"
+rsync -av www/* build/www/
+
+echo "N3BUILD: Complete"
