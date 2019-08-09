88530d06f23d2fb0273f905e755d83ec2ae07195
Scott Penrose <scottp@dd.com.au>
2019-08-09 15:26:31 +1000

    Include branch names

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@
 #   * n3-client
 #   * n3-transport
 
+DCUI_BRANCH="master"
+N3CLIENT_BRANCH="privacy-dev"
+N3TRANSPORT_BRANCH="qing-privacy-dev"
+
 set -e
 ORIGINALPATH=`pwd`
 
@@ -23,6 +27,7 @@ mkdir -p build/n3-transport
 
 echo "Creating DC-UI files"
 cd ../DC-UI
+git checkout $DCUI_BRANCH
 npm install
 ./node_modules/.bin/quasar build
 cd $ORIGINALPATH
@@ -30,12 +35,14 @@ rsync -av ../DC-UI/dist/spa-mat/* build/www/
 
 echo "Creating N3-Client"
 cd $GOPATH/src/github.com/nsip/n3-client
+git checkout $N3CLIENT_BRANCH
 ./build.sh
 cd $ORIGINALPATH
 rsync -av $GOPATH/src/github.com/nsip/n3-client/build/* build/n3-client/
 
 echo "Creating N3-Transport"
 cd $GOPATH/src/github.com/nsip/n3-transport
+git checkout $N3TRANSPORT_BRANCH
 ./build.sh
 cd $ORIGINALPATH
 rsync -av $GOPATH/src/github.com/nsip/n3-transport/build/* build/n3-transport/
