e5b704b358dd99c361c7d4c6cd7f6127a234a379
Scott Penrose <scottp@dd.com.au>
2019-10-09 16:12:26 +1100

    Build for one archi

--- build.sh
+++ build.sh
@@ -3,6 +3,22 @@
 # build.sh
 
 VERSION="v0.0.0";
+OSNAME=""
+
+# Simplified - assumes all 64 bit for now
+case "$OSTYPE" in
+  linux*)   OSNAME="Linux64" ;;
+  Linux*)   OSNAME="Linux64" ;;
+  darwin*)  OSNAME="Mac" ;;
+  Darwin*)  OSNAME="Mac" ;;
+  win*)     OSNAME="Windows64" ;;
+  Win*)     OSNAME="Windows64" ;;
+esac
+
+if [ "$OSNAME" == "" ]; then
+  echo "Unknown operating system from $OSTYPE"
+  exit 1
+fi
 
 # creates a all build of the n3 components and combined into
 # ZIP files for release.
@@ -12,7 +28,7 @@ VERSION="v0.0.0";
 #   * n3-client
 #   * n3-transport
 
-echo "N3BUILD: Start"
+echo "N3BUILD: Start - Building n3-$OSNAME-$VERSION.zip"
 
 DEPENDS=("git" "zip" "unzip" "npm" "node")
 for b in ${DEPENDS[@]}; do
@@ -38,9 +54,6 @@ echo "N3BUILD NOTE: removing existing builds"
 rm -rf build
 
 mkdir -p build
-mkdir -p build/Mac
-mkdir -p build/Linux64
-mkdir -p build/Windows64
 
 echo "N3BUILD: Creating N3-WEB @ $N3WEB_BRANCH"
 go get github.com/nsip/n3-web || true
@@ -49,21 +62,10 @@ git checkout $N3WEB_BRANCH
 git pull
 cd server/n3w
 go get
-
-echo "N3BUILD: building N3-WEB Mac"
-GOOS="darwin" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS"
-rsync -av * $ORIGINALPATH/build/Mac/
-rm $ORIGINALPATH/build/Mac/server.go
-
-echo "N3BUILD: building N3-WEB Linux64"
-GOOS="linux" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS"
-rsync -av * $ORIGINALPATH/build/Linux64/
-rm $ORIGINALPATH/build/Linux64/server.go
-
-echo "N3BUILD: building N3-WEB Windows64"
-GOOS="windows" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS"
-rsync -av * $ORIGINALPATH/build/Windows64/
-rm $ORIGINALPATH/build/Windows64/server.go
+echo "N3BUILD: building N3-WEB"
+go build -ldflags="$LDFLAGS"
+rsync -av * $ORIGINALPATH/build/
+rm $ORIGINALPATH/build/server.go
 
 cd $ORIGINALPATH
 
@@ -77,12 +79,8 @@ npm install
 ./node_modules/.bin/quasar build
 rsync -av src/assets dist/spa-mat/
 cd $ORIGINALPATH
-mkdir -p build/Mac/publilc/dc-ui
-mkdir -p build/Linux64/publilc/dc-ui
-mkdir -p build/Windows64/publilc/dc-ui
-rsync -av ../DC-UI/dist/spa-mat/* build/Mac/publilc/dc-ui/
-rsync -av ../DC-UI/dist/spa-mat/* build/Linux64/publilc/dc-ui/
-rsync -av ../DC-UI/dist/spa-mat/* build/Windows64/publilc/dc-ui/
+mkdir -p build/publilc/dc-ui
+rsync -av ../DC-UI/dist/spa-mat/* build/publilc/dc-ui/
 
 echo "N3BUILD: Creating DC-Dynamic files @ $DCDYNAMIC_BRANCH"
 cd ../dc-dynamic
@@ -92,12 +90,8 @@ npm install
 ./node_modules/.bin/quasar build
 rsync -av src/assets dist/spa-mat/
 cd $ORIGINALPATH
-mkdir -p build/Mac/publilc/dc-dynamic
-mkdir -p build/Linux64/publilc/dc-dynamic
-mkdir -p build/Windows64/publilc/dc-dynamic
-rsync -av ../dc-dynamic/dist/spa-mat/* build/Mac/publilc/dc-dynamic/
-rsync -av ../dc-dynamic/dist/spa-mat/* build/Linux64/publilc/dc-dynamic/
-rsync -av ../dc-dynamic/dist/spa-mat/* build/Windows64/publilc/dc-dynamic/
+mkdir -p build/publilc/dc-dynamic
+rsync -av ../dc-dynamic/dist/spa-mat/* build/publilc/dc-dynamic/
 
 #echo "N3BUILD: Generating/Updating WWW files"
 #rsync -av www/* build/www/
